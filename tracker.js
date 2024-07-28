f6c446c8e8740a8e4df5df5e955e920d21d06458
Milkiyas <milkiyashailub@gmail.com>
2024-07-28 23:52:15 +0300

    res Type

--- tracker.js
+++ tracker.js
@@ -117,3 +117,8 @@ function parseAnnounceResp(resp) {
       })
     }
   }
+  function respType(resp) {
+    const action = resp.readUInt32BE(0);
+    if (action === 0) return 'connect';
+    if (action === 1) return 'announce';
+  }
