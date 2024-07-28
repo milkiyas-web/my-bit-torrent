d15edefa94d0bd530cbd4ab7615abd0d16c47b3a
Milkiyas <milkiyashailub@gmail.com>
2024-07-28 23:58:48 +0300

    added download

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 const fs = require('fs');
 const bncode = require('bncode');
 const tracker = require('./tracker');
+const download = require('./src/download');
+const torrentParser = require('./src/torrent-parser');
 
+const torrent = torrentParser.open(process.argv[2]);
+
+download(torrent);
 const data = fs.readFileSync('puppy.torrent');
-const torrent = bncode.decode(data);
 
 tracker.gerPeers(torrent, peers => {
     console.log('list of peers', peers)
